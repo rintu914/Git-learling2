073d3c82c7d46761fba60c767d9ab68c4ff7c1b4
Rintu sasmal <rintu.sasmal.2003@gmail.com>
2026-04-05 20:20:24 +0530

    new branch payment added

--- frist.js
+++ frist.js
@@ -2,5 +2,10 @@ const button ="added a button";
 consolr.log(button);
 const login ="login page added";
 console.log("login");
+<<<<<<< HEAD
 const footer="fotter added also";
 console.log(footer);
+=======
+const upi="intregate upi also";
+console.log(upi);
+>>>>>>> a2bfcc2 (new branch payment added)
