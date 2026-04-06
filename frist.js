ca5ac71daac56b1de0280ae37fd5b9ffebd82ec6
Rintu sasmal <rintu.sasmal.2003@gmail.com>
2026-04-06 10:40:05 +0530

    commit after rebace

--- frist.js
+++ frist.js
@@ -2,15 +2,9 @@ const button ="added a button";
 consolr.log(button);
 const login ="login page added";
 console.log("login");
-<<<<<<< HEAD
 const footer="fotter added also";
 console.log(footer);
-=======
 const upi="intregate upi also";
 console.log(upi);
-<<<<<<< HEAD
->>>>>>> a2bfcc2 (new branch payment added)
-=======
-const cash ="payment intregated by cash also";
-console.log(cash);
->>>>>>> 9b1e235 (cash adde in payment branch)
+const gitpush="2nd time push";
+console.log(gitpush);
